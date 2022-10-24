6e39972f9d8b020cecc44d3bff2186e3027bbf7b
Ista Utami <istau41@gmail.com>
2022-10-24 12:10:41 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    echo "Hello World!<br>";
+
+    $nama = "Ista";
+    $umur = 22;
+    echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+    $namaAdik = "Utami";
+    $umurAdik = 12;
+    echo "Nama adik saya <strong>$namaAdik</strong>, saya berusia $umurAdik tahun.<br>";
+
+    $selisihUmur = $umur - $umurAdik;
+    echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun"
+
+?>
